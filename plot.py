f3286e53c135c0e1f5444070efb65b8dae8e9101
Tanguy Fardet <tanguyfardet@protonmail.com>
2022-09-22 11:14:01 +0200

    Fix plot for shapely 2.0 and bump version

--- plot.py
+++ plot.py
@@ -4,17 +4,17 @@
 # This file is part of the PyNCulture project, which aims at providing tools to
 # easily generate complex neuronal cultures.
 # Copyright (C) 2017 SENeC Initiative
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -83,7 +83,7 @@ def plot_shape(shape, axis=None, m='', mc="#999999", fc="#8888ff",
 
     # plot the main shape
     if isinstance(shape, MultiPolygon):
-        for p in shape:
+        for p in shape.geoms:
             plot_shape(p, axis=axis, m=m, mc=mc, fc=fc, ec=ec, alpha=alpha,
                        brightness=brightness, show=False,
                        show_contour=show_contour, **kwargs)
